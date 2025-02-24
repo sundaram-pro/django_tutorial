5c5bd1bd00274abd04d855203b65388f00b3a549
sundaram.pro <sundaram.pro@outlook.com>
2025-02-25 00:26:44 +0530

    model fix

--- lesson1/myproject/posts/models.py
+++ lesson1/myproject/posts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    title= models.CharField(max_lenght=75)
+    title= models.CharField(max_length=75)
     body=models.TextField()
     slug=models.SlugField()
     date = models.DateTimeField(auto_now_add=True)
