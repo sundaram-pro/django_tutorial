bcb3d6e7369e9136b75758dd5acbd205d04b2b4b
sundaram.pro <sundaram.pro@outlook.com>
2025-03-01 23:58:44 +0530

    pages, url and slug

--- lesson1/myproject/posts/urls.py
+++ lesson1/myproject/posts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.posts_list)
+    path('', views.posts_list, name="posts"),
+    path('<slug:slug>', views.post_page, name="page"),
 ]
